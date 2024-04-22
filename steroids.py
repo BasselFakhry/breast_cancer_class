4050a63884d9feba44beb37e0e52f7f4e5df6aaf
MoustaphaGh30 <mmg30@mail.aub.edu>
2024-04-22 10:54:45 +0300

    Added Accuracy Score measurement

--- steroids.py
+++ steroids.py
@@ -2,12 +2,13 @@ import deep_wide
 import pandas as pd
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 import numpy as np
 
 
 df=pd.read_csv('data.csv')
 X, y=deep_wide.data_preprocess(df)
+
 
 X_train, X_test, y_train, y_test =train_test_split(X,y, test_size=0.2)
 dtrain=xgb.DMatrix(X_train, label=y_train)
@@ -36,3 +37,6 @@ print("Confusion matrix: ")
 print(f"{labels[0]}: {tn}, {labels[1]}: {fp}")
 print(f"{labels[2]}: {fn}, {labels[3]}: {tp}")
 
+
+# Print accuracy score
+print("Accuracy Score:", accuracy_score(y_test,predictions))  
